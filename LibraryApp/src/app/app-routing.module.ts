e0761178e3cf6c5e56dcb5fda1d6e40f41c85c1e
Mangesh <mangesh05in@gmail.com>
2022-01-28 16:01:13 -0500

    commit 0128

--- LibraryApp/src/app/app-routing.module.ts
+++ LibraryApp/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { AuthModule } from './auth/auth.module';
+import { PublicComponent } from './public/public.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'user',
+    component:UserComponent,
+    loadChildren:() =>import('./user/user.module').then((x)=>x.UserModule),
+  },
+  {
+    path:'public',
+    component:PublicComponent,
+    loadChildren:() =>import('./public/public.module').then((x)=>x.PublicModule),
+  },
+  {
+    path:'auth',
+    component:AuthComponent,
+    loadChildren:() =>import('./auth/auth.module').then((x)=>x.AuthModule),
+  },
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
